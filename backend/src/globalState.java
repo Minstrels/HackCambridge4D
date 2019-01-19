02c81fe99d36c17562cfd5ba252fe74aa107c8a6
Niall Egan <negan1998@gmail.com>
2019-01-19 14:25:24 +0000

    Added representation of state

---
+++ backend/src/globalState.java
@@ -0,0 +1,7 @@
+
+public class GlobalState {
+  public Map<Integer, Map<String, Integer>> cardHands;
+  public List<String> deck;
+  public Map<String, Integer> discardPile;
+  public Map<Integer, Boolean> playersAlive;
+}
---
+++ backend/src/individualState.java
@@ -0,0 +1,8 @@
+
+public class IndividualState {
+  public Map<String, Integer> discardPile;
+  public Map<Integer, Integer> numberOfCards;
+  public Map<String, Integer> hand;
+  public Map<Integer, Boolean> playersAlive;
+  
+}
