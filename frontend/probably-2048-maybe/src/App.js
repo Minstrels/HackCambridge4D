54c23067d607b8e39590be0729c3230c040de9e5
macro206 <mpozzi@hotmail.co.uk>
2019-01-19 20:48:56 +0000

    Finished frontend

--- frontend/probably-2048-maybe/src/App.js
+++ frontend/probably-2048-maybe/src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Grid from './Grid.js';
 
 class App extends Component {
+
+  handleKeyPress(event) {
+    if (event.keyCode === 37) {
+      console.log("Left");
+    }
+    else if (event.keyCode === 38) {
+      console.log("Up");
+    }
+    else if (event.keyCode === 39) {
+      console.log("Right");
+    }
+    else if (event.keyCode === 40) {
+      console.log("Down");
+    }
+  }
+
+  componentDidMount(){
+    document.addEventListener("keydown", this.handleKeyPress, false);
+  }
+  componentWillUnmount(){
+    document.removeEventListener("keydown", this.handleKeyPress, false);
+  }
+
   render() {
     return (
-      <div className="App">
-        <Grid cells={[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]} cellSize={"14vh"} />
+      <div className="App" onKeyPress={(event) => this.handleKeyPress(event)}>
+        <Grid cells={[[1024,512,32,2],[2048,8,128,64],[0,2,16,256],[4,2,4,2]]} cellSize={"14vh"} />
       </div>
     );
   }
--- frontend/probably-2048-maybe/src/Grid.js
+++ frontend/probably-2048-maybe/src/Grid.js
@@ -1,11 +1,61 @@
 import React from 'react';
 import './Grid.css';
 
+const getIndexForValue = (val) => (val === 0 ? 0 : Math.log2(val));
+
+function getColourForCellValue(val) {
+  let index = getIndexForValue(val);
+
+  switch (index) {
+    case 0:
+      return "#bbb";
+    case 1:
+      return "#eee";
+    case 2:
+      return "#e0e0fa";
+    case 3:
+      return "#a0a0f0";
+    case 4:
+      return "#6060ff";
+    case 5:
+      return "#2040ff";
+    case 6:
+      return "#0090a4";
+    case 7:
+      return "#00c090";
+    case 8:
+      return "#00c020";
+    case 9:
+      return "#00dd00";
+    case 10:
+      return "#ffbb00";
+    case 11:
+      return "#ff5000";
+    default:
+      return "#bbb";
+  }
+}
+
+function getTextColourForCellValue(val) {
+  let index = getIndexForValue(val);
+
+  if (index > 3) return "#eee";
+  else return "#444";
+}
+
+const Cell = (props) => (
+  <div style={{width: props.cellSize, height: props.cellSize, backgroundColor: getColourForCellValue(props.value)}}
+       className="Grid-cell">
+    <label style={{color: getTextColourForCellValue(props.value)}}
+           className="Grid-cell-label">{props.value === 0 ? "" : props.value}</label>
+  </div>
+);
+
 const Grid = (props) => (
   <div className="Grid">
     {props.cells.map((row) => (
       <div className="Grid-row">
-        {row.map((val) => <div style={{width: props.cellSize, height: props.cellSize, border: "5px solid black"}} />)}
+        {row.map((val) => <Cell cellSize={props.cellSize} value={val} />)}
       </div>
     ))}
   </div>
