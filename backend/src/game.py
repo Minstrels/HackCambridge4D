baf502a8d5b5ad34a6e5c878e6b943a70e0e042a
Joshua Wharton <jfw48@cam.ac.uk>
2019-01-19 20:23:41 +0000

    Finished everytrhing

--- backend/src/game.py
+++ backend/src/game.py
@@ -1,6 +1,12 @@
 class Game:
-    cells = [[0 for _ in range(4)] for _ in range(4)]
+    def __init__():
+        self.cells = [[0 for _ in range(4)] for _ in range(4)]
 
+    @staticmethod
+    def tighten(row):
+        new_row = [i for i in row if i!=0]
+        new_row = 
+    
     def shiftLeft(self):
         pass
 
