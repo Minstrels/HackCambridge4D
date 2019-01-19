43df8782f5853cc3da5cfb69cce3e8a12e2419ac
Jack Deeley <jack.deeley@ntlworld.com>
2019-01-19 14:49:41 +0000

    Added java.util import and simple constructors for state classes.

--- backend/src/GlobalState.java
+++ backend/src/GlobalState.java
@@ -5,4 +5,18 @@ public class GlobalState {
     public List<String> deck;
     public Map<String, Integer> discardPile;
     public Map<Integer, Boolean> playersAlive;
+
+    public GlobalState(Integer numPlayers){
+        cardHands = new HashMap<>();
+        deck = new LinkedList<>();
+        discardPile = new HashMap<>();
+        playersAlive = new HashMap<>();
+
+        for(int i = 0; i < numPlayers; i++){
+            cardHands.put(i, new HashMap<>()); // Initialise all Maps.
+            playersAlive.put(i, true);
+        }
+
+    }
+
 }
--- backend/src/IndividualState.java
+++ backend/src/IndividualState.java
@@ -1,8 +1,21 @@
+import java.util.*;
 
 public class IndividualState {
-  public Map<String, Integer> discardPile;
-  public Map<Integer, Integer> numberOfCards;
-  public Map<String, Integer> hand;
-  public Map<Integer, Boolean> playersAlive;
-  
+    public Map<String, Integer> discardPile;
+    public Map<Integer, Integer> numberOfCards;
+    public Map<String, Integer> hand;
+    public Map<Integer, Boolean> playersAlive;
+
+    public IndividualState(Integer numPlayers){
+        discardPile = new HashMap<>();
+        numberOfCards = new HashMap<>();
+        hand = new HashMap<>();
+        playersAlive = new HashMap<>();
+
+        /*for(int i = 0; i < numPlayers){
+
+        }*/
+
+    }
+
 }
