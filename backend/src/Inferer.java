28f7075f629b9f0d8fa099ef54b2ee10cb8a917a
Niall Egan <negan1998@gmail.com>
2019-01-19 15:08:26 +0000

    Added inferer class

---
+++ backend/src/Inferer.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Inferer {
+  Probabilities infer(IndividualState state) {
+    // todo
+    return new Probabilities(new ArrayList<Map<String, Double>>(),
+                             new HashMap<String, List<Double>>());
+  }
+}
