0e8d2b50a88a347681a0b72d3b782fa1e8d04ffe
Jack Deeley <jack.deeley@ntlworld.com>
2019-01-19 15:02:33 +0000

    IndividualState now takes arguments for each of its values; includes playerID field for represented player.

--- backend/src/IndividualState.java
+++ backend/src/IndividualState.java
@@ -1,21 +1,19 @@
 import java.util.*;
 
 public class IndividualState {
+    public Integer playerID;
     public Map<String, Integer> discardPile;
     public Map<Integer, Integer> numberOfCards;
     public Map<String, Integer> hand;
     public Map<Integer, Boolean> playersAlive;
 
-    public IndividualState(Integer numPlayers){
-        discardPile = new HashMap<>();
-        numberOfCards = new HashMap<>();
-        hand = new HashMap<>();
-        playersAlive = new HashMap<>();
-
-        /*for(int i = 0; i < numPlayers){
-
-        }*/
-
+    public IndividualState(Map<String,Integer> discard, Map<Integer, Integer> cardCounts,
+                            Map<String, Integer> myHand, Map<Integer, Boolean> liveStatus, Integer id){
+        discardPile = discard;
+        numberOfCards = cardCounts;
+        hand = myHand;
+        playersAlive = liveStatus;
+        playerID = id;
     }
 
 }
