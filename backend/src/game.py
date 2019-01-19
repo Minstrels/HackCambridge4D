9461968097444c577a5b6a0cccba654a78398679
Niall Egan <negan1998@gmail.com>
2019-01-19 19:53:22 +0000

    Basic game class

---
+++ backend/src/game.py
@@ -0,0 +1,17 @@
+class Game:
+    cells= [[0 for _ in range(4)] for _ in range(4)]
+
+    def shiftLeft():
+        pass
+
+    def shiftRight():
+        pass
+
+    def shiftUp():
+        pass
+
+    def shiftDown():
+        pass
+
+    def addValue():
+        pass
