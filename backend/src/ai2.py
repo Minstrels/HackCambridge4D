dccd7da2db9192274100759464419defb3d69188
Joshua Wharton <jfw48@cam.ac.uk>
2019-01-19 23:06:27 +0000

    Deepmind 3.0

--- backend/src/ai2.py
+++ backend/src/ai2.py
@@ -1,47 +1,39 @@
-import Game
+from game import *
 
-class AI:
-    def __init__(self,cells):
-        self.score = 0
-        self.cells = cells
-        self.game = Game(self.cells)
+def expectiMax(game, depth, isPlayer):
+    if depth == 0 or game.availableCells() == 0:
+        return game.score,-1
 
-    def expectiMax(self, game, depth, isPlayer):
-        previousState = game.cells
-        if depth == 0 or len(game.availableCells) == 0:
-            return game.score(),-1
+    if isPlayer:
+        maxValue = -1
+        direction = ''
+        for newGame in game.possibleMoves():
+            value = expectiMax(newGame[0], depth, not isPlayer)
+            maxValue = max(value,maxValue)
 
-        if isPlayer:
-            maxValue = -1
-            for i in range(4):
-                newGame = self.nextMove(previousState,i)
-                value = self.expectiMax(newGame, depth, not isPlayer)
-                maxValue = max(value,maxValue)
-            return maxValue,i
-
+            if value == maxValue:
+                direction = newGame[1]
         
-        else:
-            expectedValue = 0
-            availableCells = game.availableCells()
-            size = len(availableCells)
-            for i in range(size):
-                cells = previousState
-                cells[availableCells[i]] = 2
-                newGame = Game(cells)
-                expectedValue += (1.0 / size) * 0.9 * self.expectiMax(newGame, depth - 1, not isPlayer)[0]
-                
-                cells[availableCells[i]] = 4
-                newGame = Game(cells)
-                expectedValue += (1.0 / size) * 0.1 * self.expectiMax(newGame, depth - 1, not isPlayer)[0]
-            
-            return expectedValue
 
-    def nextMove(self,previousState,direction):
-        if direction == 0:
-            return Game(previousState).shiftLeft()
-        elif direction == 1:
-            return Game(previousState).shiftRight()
-        elif direction == 2:
-            return Game(previousState).shiftUp()
-        else:
-            return Game(previousState).shiftDown()
+        return maxValue,direction
+
+    else:
+        expectedValue = 0
+        availableCells = game.availableCells()
+        size = availableCells
+        for newGame in game.possibilities(2):
+            expectedValue += (1.0 / size) * 0.9 * expectiMax(newGame, depth - 1, not isPlayer)[0]
+        for newGame in game.possibilities(4):
+            expectedValue += (1.0 / size) * 0.1 * expectiMax(newGame, depth - 1, not isPlayer)[0]
+        
+        return expectedValue
+
+
+if __name__ == '__main__':
+    g = Game()
+    g.cells = [[0, 0, 8, 0],
+               [2, 2, 0, 4],
+               [0, 0, 8, 0],
+               [4, 2, 2, 4]]
+    maxval,direct = expectiMax(g,3,True)
+    print(direct)
