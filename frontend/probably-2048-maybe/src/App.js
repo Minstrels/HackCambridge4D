7f5aa22c50447d2f20db4dae7d00516134ab480a
macro206 <mpozzi@hotmail.co.uk>
2019-01-20 10:03:39 +0000

    Added arrows

--- frontend/probably-2048-maybe/src/App.js
+++ frontend/probably-2048-maybe/src/App.js
@@ -14,24 +14,27 @@ class App extends Component {
       score: 0,
       cells: [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],
       gameOver: false,
-      recommendedDirection: "Up",
+      recommendedDirections: [0,0,0,0],
       autoPlayEnabled: false
     };
   }
 
-  getDirectionString(directionVal) {
-    if (45 <= directionVal && directionVal < 135) {
-      return "Right";
+  getBestMove(coeffs) {
+    let maxIndex = -1;
+    let maxVal = 0;
+
+    for (let i = 0; i < coeffs.length; ++i) {
+      if (coeffs[i] > maxVal) {
+        maxVal = coeffs[i];
+        maxIndex = i;
+      }
     }
-    else if (135 <= directionVal && directionVal < 225) {
-      return "Down";
-    }
-    else if (225 <= directionVal && directionVal < 315) {
-      return "Left";
-    }
-    else {
-      return "Up";
-    }
+
+    return ["up", "right", "down", "left"][maxIndex];
+  }
+
+  playBestMove() {
+    this.sendAction(this.getBestMove(this.state.recommendedDirections));
   }
 
   sendAction(action) {
@@ -42,11 +45,11 @@ class App extends Component {
           score: jsonData.gameState.score,
           cells: jsonData.gameState.squares,
           gameOver: jsonData.gameState.game_over,
-          recommendedDirection: this.getDirectionString(jsonData.direction)
+          recommendedDirections: jsonData.moves
         });
 
         if (!jsonData.gameState.game_over && this.state.autoPlayEnabled) {
-          this.sendAction(this.getDirectionString(jsonData.direction).toLowerCase());
+          this.playBestMove();
         }
       });
   }
@@ -85,14 +88,20 @@ class App extends Component {
         </div>
 
         <div className="App-main-panels">
-          <HintPanel recommendedDirection={this.state.recommendedDirection} />
+          <HintPanel recommendedDirections={this.state.recommendedDirections} />
           <Grid cells={this.state.cells} cellSize={"14vh"} />
           <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
             <InfoPanel score={this.state.score} gameOver={this.state.gameOver} />
-            <button className="button" onClick={() => this.sendAction("new")}>New Game</button>
-            <button className="button" 
-              onClick={() => {this.setState({...this.state, autoPlayEnabled: !this.state.autoPlayEnabled}); 
-                              this.sendAction(this.getDirectionString(this.state.recommendedDirection))}} 
+            <button className="button" onClick={() => {
+              this.setState({...this.state, autoPlayEnabled: false});
+              this.sendAction("new");
+            }}>New Game</button>
+            <button className="button"
+              onClick={() => {
+                this.setState({...this.state, autoPlayEnabled: !this.state.autoPlayEnabled});
+                if (this.state.autoPlayEnabled)
+                  this.playBestMove();
+                }}
               style={{backgroundColor: this.state.autoPlayEnabled ? "#6e6" : "#e66"}}>
               Toggle AutoPlay
             </button>
--- frontend/probably-2048-maybe/src/HintPanel.js
+++ frontend/probably-2048-maybe/src/HintPanel.js
@@ -3,7 +3,18 @@ import './HintPanel.css';
 
 const HintPanel = (props) => (
   <div className="HintPanel">
-    <label className="HintPanel-label">{props.recommendedDirection}</label>
+    <div className="HintPanel-row">
+      <label style={{opacity: props.recommendedDirections[0]}} className="HintPanel-label">↑</label>
+    </div>
+
+    <div className="HintPanel-row">
+      <label style={{opacity: props.recommendedDirections[3]}} className="HintPanel-label">←</label>
+      <label style={{opacity: props.recommendedDirections[1]}} className="HintPanel-label">→</label>
+    </div>
+
+    <div className="HintPanel-row">
+      <label style={{opacity: props.recommendedDirections[2]}} className="HintPanel-label">↓</label>
+    </div>
   </div>
 );
 
